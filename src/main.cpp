0b5ac26c3a308d745dbf166c75b9a0a3d752f10a
Rubén A <sortexjuegos@gmail.com>
2023-07-23 13:05:54 -0400

    Cerrar logica dentro de la clase App

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,45 @@
-#include <iostream>
 #include <stdint.h>
+#include <iostream>
 #include <raylib-cpp.hpp>
 
 using namespace std;
 
+class App {
+ private:
+  raylib::Window window;
+ public:
+  App(int16_t scrWidth, int16_t scrHeight, const char* title);
+  ~App();
+  void Run();
+};
+
+App::App(int16_t scrWidth, int16_t scrHeight, const char* title) {
+  window = raylib::Window(scrWidth, scrHeight, title);
+  window.SetTargetFPS(60);
+}
+
+App::~App() {
+  window.Close();
+}
+
+void App::Run() {
+  while (!window.ShouldClose()) {
+
+    window.BeginDrawing();
+    window.ClearBackground(RAYWHITE);
+
+    window.DrawFPS(20, 20);
+
+    window.EndDrawing();
+  }
+}
+
 int main(void) {
-	cout << "Hello world" << endl;
+  cout << "Hello world" << endl;
 
-	int16_t scrWidth = 960, scrHeight = 540;
-	raylib::Window window(scrWidth, scrHeight, "Hello world");
-	SetTargetFPS(60);
+  int16_t scrWidth = 960, scrHeight = 540;
+  App myApp(scrWidth, scrHeight, "Hello world");
 
-	while (!window.ShouldClose()) {
-		BeginDrawing();
-		window.ClearBackground(RAYWHITE);
-
-		window.DrawFPS(50, 50);
-
-		EndDrawing();
-	}
-	return 0;
+  myApp.Run();
+  return 0;
 }
