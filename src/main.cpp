baf528a72e226f854faf91673729fbe1b3234e79
Rubén A <sortexjuegos@gmail.com>
2023-07-25 19:00:43 -0400

    Añadir la entidad del jugador y dibujar su area

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 struct BoundingBoxComponent {
+	raylib::Vector2 halfLen;
   raylib::Rectangle bb;
 
   BoundingBoxComponent() = default;
@@ -15,6 +16,16 @@ struct BoundingBoxComponent {
   operator const raylib::Rectangle&() const { return bb; };
 };
 
+struct PositionComponent {
+	raylib::Vector2 pos;
+	
+  PositionComponent() = default;
+  PositionComponent(const PositionComponent&) = default;
+  PositionComponent(const raylib::Vector2& vec2) : pos(vec2) {};
+
+  operator const raylib::Vector2&() const { return pos; };
+};
+
 class App {
  private:
   raylib::Window window;
@@ -22,7 +33,7 @@ class App {
   entt::entity player;
   entt::entity ground;
   entt::entity obstacle;
-  entt::registry m_registry;
+  entt::registry m_reg;
  public:
   App(int16_t scrWidth, int16_t scrHeight, const char* title);
   ~App();
@@ -32,6 +43,15 @@ class App {
 App::App(int16_t scrWidth, int16_t scrHeight, const char* title) {
   window.Init(scrWidth, scrHeight, title, 0U);
   window.SetTargetFPS(60);
+
+	player = m_reg.create();
+	// ground = m_reg.create();
+	// obstacle = m_reg.create();
+	auto p_pos = raylib::Vector2(50.0, 50.0);
+	auto p_rect = raylib::Rectangle(p_pos, raylib::Vector2(120.0, 120.0));
+
+	m_reg.emplace<PositionComponent>(player, p_pos);
+	m_reg.emplace<BoundingBoxComponent>(player, p_rect);
 }
 
 App::~App() {
@@ -43,6 +63,11 @@ void App::Run() {
 
     window.BeginDrawing();
     window.ClearBackground(RAYWHITE);
+
+		for (auto entity: m_reg.view<BoundingBoxComponent>()) {
+			auto &bb = m_reg.get<BoundingBoxComponent>(entity);
+			DrawRectangleRec(bb, raylib::Color::Blue());
+		}
 
     window.DrawFPS(20, 20);
 
