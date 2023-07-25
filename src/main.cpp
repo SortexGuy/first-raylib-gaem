66b293f57f025f96bb21f354d1dc5e1ad8612200
Rubén A <sortexjuegos@gmail.com>
2023-07-25 15:54:26 -0400

    Añadido Entity/Componen/System y EnTT como dep

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,28 @@
 #include <stdint.h>
 #include <iostream>
 #include <raylib-cpp.hpp>
+#include <entt/entt.hpp>
 
 using namespace std;
+
+struct BoundingBoxComponent {
+  raylib::Rectangle bb;
+
+  BoundingBoxComponent() = default;
+  BoundingBoxComponent(const BoundingBoxComponent&) = default;
+  BoundingBoxComponent(const raylib::Rectangle& rect) : bb(rect) {};
+
+  operator const raylib::Rectangle&() const { return bb; };
+};
 
 class App {
  private:
   raylib::Window window;
+
+  entt::entity player;
+  entt::entity ground;
+  entt::entity obstacle;
+  entt::registry m_registry;
  public:
   App(int16_t scrWidth, int16_t scrHeight, const char* title);
   ~App();
